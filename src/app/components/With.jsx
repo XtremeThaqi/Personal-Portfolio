181cfb2c73fd2328e4befe0d9e52fc500fc27113
XtremeThaqi <erblinthaqi40@gmail.com>
2024-07-30 17:06:57 +0200

    added links to the with section

--- src/app/components/With.jsx
+++ src/app/components/With.jsx
@@ -8,21 +8,21 @@ export default function With() {
     <>
         <div className="relative overflow-hidden">
             <div className='flex flex-col items-center'>
-                <h1 className="text-[40px] font-bold text-red-600 relative z-10"><FaTools/></h1>
+               <FaTools className='text-[40px] font-bold text-red-600 relative z-10'/>
                 <span className='text-red-600 mt-5 text-[30px] animate-bounce'>&darr;</span>
                 <div className="flex flex-wrap justify-center my-4 border border-red-600 p-5 rounded-full">
-                    <div className="content">
+                    <a href='https://developer.mozilla.org/en-US/docs/Web/HTML' target='_blank' className="content">
                         <FaHtml5 className="text-[30px] text-red-600 max-md:text-[25px]"/>
-                    </div>
-                    <div className="content">
+                    </a>
+                    <a href='https://tailwindcss.com' target='_blank' className="content">
                         <RiTailwindCssFill className="text-[30px] text-red-600 max-md:text-[25px]"/>
-                    </div>
-                    <div className="content">
+                    </a>
+                    <a href='https://react.dev/' target='_blank' className="content">
                         <FaReact className="text-[30px] text-red-600 max-md:text-[25px]"/>
-                    </div>
-                    <div className="content">
+                    </a>
+                    <a href='https://nextjs.org/' target='_blank' className="content">
                         <RiNextjsFill className="text-[30px] text-red-600 max-md:text-[25px]"/>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
