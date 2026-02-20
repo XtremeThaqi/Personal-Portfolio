d60d183b6c21c982c32bc9ad2e2807aeee50af5a
XtremeThaqi <erblinthaqi40@gmail.com>
2026-02-20 03:30:24 +0100

    Update Home.jsx

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -13,8 +13,6 @@ export const socialLinks = [
 ];
 
 export default function Home() {
-
-
     return (
         <section id="home" className="relative min-h-screen bg-gradient-to-b from-black via-zinc-950 to-black flex items-center justify-center px-5 sm:px-8 overflow-hidden mx-auto">
             <div className="z-10 max-w-6xl pt-28">
